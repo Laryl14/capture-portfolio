4e5b53c85d55bc7a7f1a4c8fb9d4a487c0e187f7
Laryl Li <larylli@Laryls-MacBook-Pro.local>
2020-12-30 18:11:12 -0800

    styled component aboutsection

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,34 +1,68 @@
 import React from "react";
 import home1 from "../img/home1.png";
+//styled
+import styled from "styled-components";
 
 const AboutSection = () => {
   return (
-    <div>
-      <div className="description">
+    <About className="about">
+      <StyledDescription className="description">
         <div className="title">
-          <div className="hide">
+          <StyledHide className="hide">
             <h2>We work to Make</h2>
-          </div>
-          <div className="hide">
+          </StyledHide>
+          <StyledHide className="hide">
             <h2>
               your <span>dreams</span> come
             </h2>
-          </div>
-          <div className="hide">
+          </StyledHide>
+          <StyledHide className="hide">
             <h2>true.</h2>
-          </div>
+          </StyledHide>
         </div>
         <p>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills.
         </p>
         <button>Contact Us</button>
-      </div>
-      <div className="image">
+      </StyledDescription>
+      <StyledImage className="image">
         <img src={home1} alt="camera man" />
-      </div>
-    </div>
+      </StyledImage>
+    </About>
   );
 };
+
+//Styled Components - backticks / styled components look like components
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  padding: 5rem 10rem;
+  color: white;
+`;
+
+const StyledDescription = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const StyledImage = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover; //stretches back out and keeps aspect ratio
+  }
+`;
+
+const StyledHide = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutSection;
